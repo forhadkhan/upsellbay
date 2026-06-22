7d0c9577c5b57be973986b1ecc5b00bb04033bcd
Forhad Khan <forhadkhan@tuta.io>
2026-06-22 17:35:19 +0600

    chore: trim plugin header whitespace

--- upsellbay.php
+++ upsellbay.php
@@ -3,7 +3,7 @@
  * Plugin Name: UpsellBay
  * Plugin URI: https://wpanchorbay.com/upsellbay/
  * Description: WooCommerce-native order bumps and AOV offers for product, cart, checkout, and thank-you journeys.
- * Version: 1.0.0 
+ * Version: 1.0.0
  * Requires at least: 6.9
  * Requires PHP: 8.1
  * Author: WPAnchorBay
